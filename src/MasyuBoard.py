ceda33f2d28b87de629a4ed52b583eed65cb41c6
opussf <github@zz9-za.com>
2017-12-16 03:15:37 -0800

    Build changes, initial src files

---
+++ src/MasyuBoard.py
@@ -0,0 +1,11 @@
+#########################################
+# Project:    Masyu Solver
+# Filename:   MasyuBoard.py
+# Author:     Opussf
+# Started:    16 Dec 2017
+#########################################
+
+class MasyuBoard( object ):
+	"""Masyu Board object
+	"""
+	pass
