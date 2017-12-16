7059b5a1db8def3ac03595f7b10213b514011557
opussf <github@zz9-za.com>
2017-12-16 04:32:31 -0800

    Tests

--- test/TestMasyuBoard.py
+++ test/TestMasyuBoard.py
@@ -8,7 +8,6 @@ class TestMasyuBoard(unittest.TestCase):
 		#self.myBoard = SudokuBoard(9)
 		#self.myBoard.initBoard( "5,,,,6,,7,,,,,,,7,8,,1,3,7,,9,5,,,6,8,,,9,,,,3,1,,5,3,,,8,,6,,,4,4,,5,7,,,,6,,,5,2,,,7,4,,6,8,6,,4,9,,,,,,,4,,2,,8,,7" )
 		#self.myBoard.loadFromFile("sudokuboard.puzzle.test")
-		pass
 	def test_initBoard_01( self ):
 		""" initBoard with a blank puzzle, size """
 		self.masyuBoard.initBoard( 3 )
@@ -17,8 +16,10 @@ class TestMasyuBoard(unittest.TestCase):
 		self.masyuBoard.initBoard( "...\n...\n..." )
 	def test_initBoard_03( self ):
 		""" initBoard from a file """
-		#self.masyuBoard.loadFromFile("puzzles/puzzle_2.txt")
-		pass
+		self.masyuBoard.loadFromFile( "puzzles/puzzle_0.txt" )
+	def test_Print(self):
+		""" fails if not string """
+		self.assertEquals( type( self.masyuBoard.__str__() ), type( "" ), "Should return a string" )
 
 def suite():
 	suite = unittest.TestSuite()
