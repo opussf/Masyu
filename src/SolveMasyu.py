2be128eae00dcff4b31d41025cdee5fa0e2bb3a3
opussf <github@zz9-za.com>
2018-01-21 13:36:37 -0800

    Adding a logger, black dots based on noExit flags, not location.

--- src/SolveMasyu.py
+++ src/SolveMasyu.py
@@ -8,6 +8,7 @@
 #
 
 import MasyuBoard
+import logging
 #import time
 
 class SolveMasyu( object ):
@@ -25,82 +26,134 @@ class SolveMasyu( object ):
 		else:
 			# do something here?
 			pass
+		self.logger = logging.getLogger( "SolveMasyu" )
+		self.logger.setLevel( logging.DEBUG )
+		handler = logging.FileHandler( "SolveMasyu.log" )
+		handler.setLevel( logging.DEBUG )
+		formatter = logging.Formatter( "%(asctime)s %(levelname)s %(message)s" )
+		handler.setFormatter( formatter )
+		self.logger.addHandler( handler )
+		self.logger.info( "SolveMasyu __init__ completed." )
+	def __del__( self ):
+		self.logger.info( "Shutting down" )
+		self.logger.shutdown()
 	def solveBoard( self ):
 		""" Loop though the functions until the board is solved, or a stalemate is found.
 		A board with multiple solutions is possible, an ambigious solution defines an ambigious puzzle.
 		I.E. it is the burden of the puzzle to create a single solution.
 		if the puzzle has a non-empty square, send it to dot() to determine the color, and call the right function.
 		"""
-		#print( "=+"*21 )
-		for y in range( self.board.ySize ):
-			for x in range( self.board.xSize ):
-				self.dot( x, y )
+		self.logger.debug( "Starting solveBoard" )
+		doAgain = True
+		counter = 0
+		while doAgain:
+			counter += 1
+			self.logger.debug( "loop #%i" % ( counter, ) )
+			doAgain = False
+			for y in range( self.board.ySize ):
+				for x in range( self.board.xSize ):
+					doAgain = self.dot( x, y ) or doAgain
+			doAgain = False
 
 	def dot( self, x, y ):
-		""" determine the color of the dot, and call the right function """
+		""" determine the color of the dot, and call the right function.
+		returns if the function did anything or not """
 		square, lines = self.board.getValue( x, y )
-		#print( square, lines )
+		self.logger.debug( "dot( %i, %i ) square: %s value: %i" % ( x, y, square, lines ) )
 		if square == "b":
-			color = self.dotBlack( x, y )
-			return color
+			return self.dotBlack( x, y )
 		elif square == "w":
 			color = self.dotWhite( x, y )
+			#self.logger.debug( "color: %s" % ( color, ) )
 			return color
 		else:
+			self.logger.debug( "empty" )
 			return "empty"
 
 	def dotBlack( self, x, y ):
 		""" a black dot has to have the line turn 90 degrees.
 		the line must go straight for 2 segments in both directions.
+
+		WSEN
+		8421
 		"""
-		print( "dotBlack( %i, %i )" % ( x, y ) )
+		self.logger.debug( "dotBlack( %i, %i )" % ( x, y ) )
+		change = False
 		possibleDirections = 0
+		impossibleDirections = self.board.getValue( x, y )[1] >> 4
 		# look for a horizontal line
-		# look EAST ( have to be atleast 2 segments from the edge to go EAST )
-		if( x < self.board.xSize - 2 ):
-			possibleDirections = possibleDirections | self.board.EAST
-			print( "Location allows going EAST." )
-		# look WEST ( have to be at least 2 segments from the edge to go WEST )
-		if( x >= 2 ):
-			possibleDirections = possibleDirections | self.board.WEST
-			print( "Location allows going WEST." )
-		# look for a vertical line
+		# look NORTH ( have at least 2 segments to travel )
+		if( not impossibleDirections & self.board.NORTH ):  # north is possible
+			self.logger.debug( "north is possible: 1" )
+			if( not self.board.getValue( x, y-1 )[1] >> 4 & self.board.NORTH ):
+				self.logger.debug( "north is possible: 2" )
+				possibleDirections = possibleDirections | self.board.NORTH
+		if( not possibleDirections & self.board.NORTH ):
+			self.logger.debug( "north is not possible. setNoExit" )
+			self.board.setNoExit( x, y, self.board.NORTH )
+		# look EAST
+		if( not impossibleDirections & self.board.EAST ):  # east is possible
+			self.logger.debug( "east is possible: 1" )
+			if( not self.board.getValue( x+1, y )[1] >> 4 & self.board.EAST ):
+				self.logger.debug( "east is possible: 2" )
+				possibleDirections = possibleDirections | self.board.EAST
+		if( not possibleDirections & self.board.EAST ):
+			self.logger.debug( "east is not possible. setNoExit" )
+			self.board.setNoExit( x, y, self.board.EAST )
 		# look SOUTH
-		if( y < self.board.ySize - 2 ):
-			possibleDirections = possibleDirections | self.board.SOUTH
-			print( "Location allows going SOUTH." )
-		# look NORTH
-		if( y >= 2 ):
-			possibleDirections = possibleDirections | self.board.NORTH
-			print( "Location allows going NORTH." )
-		print( "possible directions: %5s %5s %5s %5s" % (
+		if( not impossibleDirections & self.board.SOUTH ):  # south is possible
+			self.logger.debug( "south is possible: 1" )
+			if( not self.board.getValue( x, y+1 )[1] >> 4 & self.board.SOUTH ):
+				self.logger.debug( "south is possible: 2" )
+				possibleDirections = possibleDirections | self.board.SOUTH
+		if( not possibleDirections & self.board.SOUTH ):
+			self.logger.debug( "south is not possible. setNoExit" )
+			self.board.setNoExit( x, y, self.board.SOUTH )
+		# look WEST
+		if( not impossibleDirections & self.board.WEST ):  # west is possible
+			self.logger.debug( "west is possible: 1" )
+			if( not self.board.getValue( x-1, y )[1] >> 4 & self.board.WEST ):
+				self.logger.debug( "west is possible: 2" )
+				possibleDirections = possibleDirections | self.board.WEST
+		if( not possibleDirections & self.board.WEST ):
+			self.logger.debug( "west is not possible. setNoExit" )
+			self.board.setNoExit( x, y, self.board.WEST )
+
+		self.logger.debug( "possible directions: %5s %5s %5s %5s" % (
 			(possibleDirections & self.board.NORTH) and "NORTH" or "",
 			(possibleDirections & self.board.EAST) and "EAST" or "",
 			(possibleDirections & self.board.SOUTH) and "SOUTH" or "",
 			(possibleDirections & self.board.WEST) and "WEST" or "" ))
 
-		if( ( possibleDirections & self.board.NORTH ) and ( possibleDirections & self.board.SOUTH ) ):
-			print( "can go both NORTH and SOUTH" )
-		elif( possibleDirections & self.board.NORTH ):
-			print( "can only go NORTH" )
+		if( ( possibleDirections & self.board.NORTH ) and not ( possibleDirections & self.board.SOUTH ) ):
+			self.logger.debug( "can only go NORTH" )
 			self.board.setExit( x, y, self.board.NORTH )
 			self.board.setExit( x, y-1, self.board.NORTH )
-		elif( possibleDirections & self.board.SOUTH ):
-			print( "can only go SOUTH" )
+			self.board.setNoExit( x, y-1, self.board.EAST | self.board.WEST )
+			change = True
+		if( ( possibleDirections & self.board.SOUTH ) and not ( possibleDirections & self.board.NORTH ) ):
+			self.logger.debug( "can only go SOUTH" )
 			self.board.setExit( x, y, self.board.SOUTH )
 			self.board.setExit( x, y+1, self.board.SOUTH )
+			self.board.setNoExit( x, y+1, self.board.EAST | self.board.WEST )
+			change = True
 
 		if( ( possibleDirections & self.board.EAST ) and not ( possibleDirections & self.board.WEST ) ):
-			print( "can only go EAST" )
+			self.logger.debug( "can only go EAST" )
 			self.board.setExit( x, y, self.board.EAST )
 			self.board.setExit( x+1, y, self.board.EAST )
+			self.board.setNoExit( x+1, y, self.board.NORTH | self.board.SOUTH )
+			change = True
 		if( ( possibleDirections & self.board.WEST ) and not ( possibleDirections & self.board.EAST ) ):
-			print( "can only go WEST" )
+			self.logger.debug( "can only go WEST" )
 			self.board.setExit( x, y, self.board.WEST )
 			self.board.setExit( x-1, y, self.board.WEST )
+			self.board.setNoExit( x-1, y, self.board.NORTH | self.board.SOUTH )
+			change = True
 
-		print self.board
+		self.logger.debug( "\n%s" % self.board )
 
-		return "black"
+		return change
 	def dotWhite( self, x, y ):
+		self.logger.debug( "dotWhite( %i, %i )" % ( x, y ) )
 		return "white"
--- test/TestSolveMasyu.py
+++ test/TestSolveMasyu.py
@@ -3,22 +3,18 @@ from MasyuBoard import *
 from SolveMasyu import *
 
 class TestSolveMasyu( unittest.TestCase ):
+	Masyu = SolveMasyu( MasyuBoard.MasyuBoard() )
 	def setUp( self ):
 		""" setUp """
-		board = MasyuBoard.MasyuBoard()
-		board.loadFromFile( "puzzles/puzzle_0.txt" )
-		self.Masyu = SolveMasyu( board )
+		self.Masyu.board.loadFromFile( "puzzles/puzzle_0.txt" )
 	def test_Masyu_hasBoard( self ):
 		self.assertTrue( self.Masyu.board )
-	def test_Masyu_solveBoard( self ):
-		self.Masyu.solveBoard()
 	def test_Masyu_dot_empty( self ):
 		self.assertEquals( self.Masyu.dot( 0, 0 ), "empty" )
 	def test_Masyu_dot_black( self ):
 		self.assertEquals( self.Masyu.dot( 2, 0 ), "black" )
 	def test_Masyu_dot_white( self ):
 		self.assertEquals( self.Masyu.dot( 1, 0 ), "white" )
-
 	def test_Masyu_blackDot_vertical_01( self ):
 		""" finds a vertical component """
 		self.Masyu.dotBlack( 0, 2 )
@@ -29,9 +25,70 @@ class TestSolveMasyu( unittest.TestCase ):
 		self.Masyu.dotBlack( 0, 2 )
 		self.assertEquals( self.Masyu.board.getValue( 2, 2 )[1],
 				( self.Masyu.board.SOUTH | self.Masyu.board.EAST ) << 4 | self.Masyu.board.WEST )
-	def test_Masyu_blackDot_( self ):
-		pass
-		#self.fail( "Eh?" )
+
+	def test_Masyu_blackDot_vertical_02( self ):
+		self.Masyu.dotBlack( 2, 0 )
+		self.assertEquals( self.Masyu.board.getValue( 0, 0 )[1],
+				( self.Masyu.board.NORTH | self.Masyu.board.WEST ) << 4 | self.Masyu.board.EAST )
+	def test_Masyu_blackDot_horizontal_02( self ):
+		self.Masyu.dotBlack( 0, 2 )
+		self.assertEquals( self.Masyu.board.getValue( 2, 2 )[1],
+				( self.Masyu.board.SOUTH | self.Masyu.board.EAST ) << 4 | self.Masyu.board.WEST )
+	def test_Masyu_blackDot_single_exit_East( self ):
+		""" draws the one line it can guess at """
+		self.Masyu.board.loadFromFile( "puzzles/puzzle_5x5_single_black.txt" )
+		self.Masyu.dotBlack( 1, 2 )
+		self.assertEquals( self.Masyu.board.getValue( 2, 2 )[1],
+				( self.Masyu.board.NORTH | self.Masyu.board.SOUTH ) << 4 | self.Masyu.board.EAST | self.Masyu.board.WEST )
+	def test_Masyu_blackDot_single_noExit_West( self ):
+		""" marks a noExit on the short side """
+		self.Masyu.board.loadFromFile( "puzzles/puzzle_5x5_single_black.txt" )
+		self.Masyu.dotBlack( 1, 2 )
+		self.assertEquals( self.Masyu.board.getValue( 1, 2 )[1],
+				( self.Masyu.board.WEST << 4 ) | self.Masyu.board.EAST )
+	def test_Masyu_blackDot_single_exit_West( self ):
+		self.Masyu.board.initBoard( 5, 5, ".....\n.....\n...b.\n.....\n....." )
+		self.Masyu.dotBlack( 3, 2 )
+		self.assertEquals( self.Masyu.board.getValue( 2, 2 )[1],
+				( self.Masyu.board.NORTH | self.Masyu.board.SOUTH ) << 4 | self.Masyu.board.EAST | self.Masyu.board.WEST )
+	def test_Masyu_blackDot_single_noExit_East( self ):
+		self.Masyu.board.initBoard( 5, 5, ".....\n.....\n...b.\n.....\n....." )
+		self.Masyu.dotBlack( 3, 2 )
+		self.assertEquals( self.Masyu.board.getValue( 3, 2 )[1],
+				( self.Masyu.board.EAST << 4 | self.Masyu.board.WEST ) )
+	def test_Masyu_blackDot_single_exit_South( self ):
+		self.Masyu.board.initBoard( 5, 5, ".....\n..b..\n.....\n.....\n....." )
+		self.Masyu.dotBlack( 2, 1 )
+		self.assertEquals( self.Masyu.board.getValue( 2, 2 )[1],
+				( self.Masyu.board.EAST | self.Masyu.board.WEST ) << 4 | self.Masyu.board.NORTH | self.Masyu.board.SOUTH )
+	def test_Masyu_blackDot_single_noExit_North( self ):
+		self.Masyu.board.initBoard( 5, 5, ".....\n..b..\n.....\n.....\n....." )
+		self.Masyu.dotBlack( 2, 1 )
+		self.assertEquals( self.Masyu.board.getValue( 2, 1 )[1],
+				( self.Masyu.board.NORTH << 4 | self.Masyu.board.SOUTH ) )
+	def test_Masyu_blackDot_single_exit_North( self ):
+		self.Masyu.board.initBoard( 5, 5, ".....\n.....\n.....\n..b..\n....." )
+		self.Masyu.dotBlack( 2, 3 )
+		self.assertEquals( self.Masyu.board.getValue( 2, 2 )[1],
+				( self.Masyu.board.EAST | self.Masyu.board.WEST ) << 4 | self.Masyu.board.NORTH | self.Masyu.board.SOUTH )
+	def test_Masyu_blackDot_single_noExit_South( self ):
+		self.Masyu.board.initBoard( 5, 5, ".....\n.....\n.....\n..b..\n....." )
+		self.Masyu.dotBlack( 2, 3 )
+		self.assertEquals( self.Masyu.board.getValue( 2, 3 )[1],
+				( self.Masyu.board.SOUTH << 4 | self.Masyu.board.NORTH ) )
+	def test_Masyu_blackDot_nested( self ):
+		self.Masyu.board.initBoard( 5, 5, ".....\n...b.\n..b..\n.....\n....." )
+		self.Masyu.dotBlack( 3, 1 )
+		self.Masyu.dotBlack( 2, 2 )
+		self.assertEquals( self.Masyu.board.getValue( 0, 2 )[1],
+				( self.Masyu.board.WEST << 4 | self.Masyu.board.EAST ) )
+
+
+	def test_Masyu_SolveBoard( self ):
+		self.Masyu.board.loadFromFile( "puzzles/puzzle_10x12_hard.txt" )
+		self.Masyu.solveBoard()
+
+
 """
 	def test_Masyu_blackDot_( self ):
 		print "balckDot_"
